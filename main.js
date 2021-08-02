23b71ceffd743a74235c76fe89b2d5affa77dab3
eseo128 <34049770+eseo128@users.noreply.github.com>
2021-08-03 02:32:44 +0900

    JS : login modal fadeIn&Out

---
+++ main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+$('.btn-open').click(function () {
+    $('.modal-login').fadeIn();
+    $('body').addClass('active');
+});
+
+$('.btn-close').click(function () {
+    $('.modal-login').fadeOut();
+    $('body').removeClass('active');
+});
